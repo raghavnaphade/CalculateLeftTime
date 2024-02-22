b4156d5d5177b0150bb7e57856de2fd1b3738e9d
Raghav Naphade <raghav.naphade@waiin.com>
2024-02-22 14:46:01 +0530

    MOD: staging

--- Form1.cs
+++ Form1.cs
@@ -17,9 +17,9 @@ namespace CalculateLeftTime
         public static HubConnection SignalRConn;
         private static int WM_QUERYENDSESSION = 0x11;
 
-        public int tenantId,userId;
-        private System.Windows.Forms.Timer timer;
-        public string accessToken = "";
+        public static int tenantId,userId;
+        private static System.Windows.Forms.Timer timer;
+        public static string accessToken = "";
         public Form1()
         {
             InitializeComponent();
@@ -35,16 +35,7 @@ namespace CalculateLeftTime
             /*this.Hide()*/
             ;
         }
-      /*  private void Form1_Load(object sender, EventArgs e)
-        {
-            System.Diagnostics.Debug.WriteLine("Form1_Load");
-            this.Hide();
-            this.Visible = false;
-            this.Opacity = 0;
-            btnReadFile_Click();
-            this.ShowInTaskbar = false;
-            this.ShowIcon = false;
-        }*/
+  
 
         private void btnReadFile_Click()
         {
@@ -76,11 +67,12 @@ namespace CalculateLeftTime
                         Console.Write($" userid {userId}");
                         System.Diagnostics.Debug.WriteLine($" userid {userId}");
                         ConnectSignalRServer();
-                        TrigerSignalRServerUserStatus("SignalRAgentUserStatus");
+                      // TrigerSignalRServerUserStatus("SignalRAgentUserStatus");
                     } else
                     {
                         Console.Write($" userid/tenantid null");
                         System.Diagnostics.Debug.WriteLine($"userid/tenantid null");
+                        AddLogs(" userid/tenantid null");
                     }
                    
 
@@ -94,6 +86,7 @@ namespace CalculateLeftTime
                   //  MessageBox.Show("File not found.", "File Not Found", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     System.Diagnostics.Debug.WriteLine($" userid not found");
                     HideApp();
+                    AddLogs(" userid/tenantid null");
 
                 }
             }
@@ -156,10 +149,10 @@ namespace CalculateLeftTime
               Console.WriteLine("Hello");
               this.Hide();
           }*/
-        private async Task Form1_FormClosedAsync(object sender, FormClosedEventArgs e)
+        private static async Task Form1_FormClosedAsync(object sender, FormClosedEventArgs e)
 
         {
-            PostUserStatusToWeb();
+            PostUserStatusToWeb(5);
             Console.WriteLine("Hello Form1_FormClosed");
 
             string message = "System is shutting down or user is logging off.2 WndProc";
@@ -186,32 +179,17 @@ namespace CalculateLeftTime
             // Stop the timer
             timer.Stop();
         }
-        private async void Form1_FormClosingAsync(Object sender, FormClosingEventArgs e)
+        private static  async void Form1_FormClosingAsync(Object sender, FormClosingEventArgs e)
         {
-            PostUserStatusToWeb();
+            PostUserStatusToWeb(5);
             //In case windows is trying to shut down, don't hold the process up
             if (e.CloseReason == CloseReason.UserClosing)
             {
                
                 timer.Start();
 
-                Console.WriteLine("Hello Form1_FormClosing UserClosing");
-
-                string message = "System is shutting down or user is logging off.3 Form1_FormClosingAsync";
-
-                // Get the path to the Documents folder
-                string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-
-                // Specify the file path in the Documents folder
-                string filePath = Path.Combine(documentsPath, "shutdown_log3.txt");
-
-                // Write the message to a text file
-                using (StreamWriter writer = new StreamWriter(filePath, true))
-                {
-                    writer.WriteLine($"{DateTime.Now} - {message}");
-                }
-                System.Diagnostics.Debug.WriteLine("System is shutting down or user is logging off.3 Form1_FormClosingAsync");
                 /*  MessageBox.Show("queryendsession: this is a logoff, shutdown, or reboot UserClosing");*/
+                AddLogs("Hello Form1_FormClosing UserClosing");
             }
             // Prompt user to save his data
 
@@ -221,18 +199,9 @@ namespace CalculateLeftTime
                 Console.WriteLine("Hello Form1_FormClosing WindowsShutDown");
 
                 string message = "System is shutting down or user is logging off.4 Form1_FormClosingAsync";
-
+                AddLogs(message);
                 // Get the path to the Documents folder
-                string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-
-                // Specify the file path in the Documents folder
-                string filePath = Path.Combine(documentsPath, "shutdown_log4.txt");
-
-                // Write the message to a text file
-                using (StreamWriter writer = new StreamWriter(filePath, true))
-                {
-                    writer.WriteLine($"{DateTime.Now} - {message}");
-                }
+               
                 System.Diagnostics.Debug.WriteLine("System is shutting down or user is logging off.4 Form1_FormClosingAsync");
 
                
@@ -248,123 +217,19 @@ namespace CalculateLeftTime
             this.Opacity = 0;
             btnReadFile_Click();
         }
-        private async Task btnPost_Click()
-        {
-            try
-            {
-                string apiUrl = "https://uatapi.intimepro.io/api/services/app/AgentHelper/SignalRetOrUpdateUserStatusFromAgentAPI";
-
-                // Create an instance of HttpClient
-                using (HttpClient httpClient = new HttpClient())
-                {
-
-                    int status = 5;
-                    DateTime utcDate = DateTime.UtcNow;
-
-                    // Create JSON data
-                    string postData = $"{{\"tenantId\":\"{tenantId}\", \"userId\":\"{userId}\", \"userStatus\":\"{status}\", \"creationTime\":\"{utcDate:s}\"}}";
-
-                    httpClient.DefaultRequestHeaders.Add("Abp.TenantId", tenantId.ToString());  // Replace with your actual tenantId
-                    httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {accessToken}");
-                    // Create a StringContent object with the JSON data
-                    StringContent content = new StringContent(postData, Encoding.UTF8, "application/json");
-
-                    try
-                    {
-                        // Make the POST request
-                        HttpResponseMessage response = await httpClient.PostAsync(apiUrl, content);
-                      
-
-                        // Check if the request was successful (status code 200-299)
-                        if (response.IsSuccessStatusCode)
-                        {
-                            // Read the response content
-                            string responseContent = await response.Content.ReadAsStringAsync();
-                            string message = responseContent + "";
-
-                            // Get the path to the Documents folder
-                            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-
-                            // Specify the file path in the Documents folder
-                            string filePath = Path.Combine(documentsPath, "IsSuccessStatusCode.txt");
-
-                            // Write the message to a text file
-                            using (StreamWriter writer = new StreamWriter(filePath, true))
-                            {
-                                writer.WriteLine($"{DateTime.Now} - {message} ");
-                            }
-
-                            // Process the response as needed
-                            /*  MessageBox.Show("POST request successful. Response: " + responseContent);*/
-                        }
-                        else
-                        {
-                            // Handle the error response
-                            /*  MessageBox.Show("POST request failed. Status Code: " + response.StatusCode);*/
-                            string message = "POST request failed. Status Code: " + response.StatusCode;
-
-                            // Get the path to the Documents folder
-                            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-
-                            // Specify the file path in the Documents folder
-                            string filePath = Path.Combine(documentsPath, "IsSuccessStatusCodeFalse.txt");
-
-                            // Write the message to a text file
-                            using (StreamWriter writer = new StreamWriter(filePath, true))
-                            {
-                                writer.WriteLine($"{DateTime.Now} - {message}");
-                            }
-                        }
-                    }
-                    catch (Exception ex)    
-                    {
-                        // Handle any exceptions that occur during the request
-                        string message = ex.Message + "sec last Exp";
-
-                        // Get the path to the Documents folder
-                        string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-
-                        // Specify the file path in the Documents folder
-                        string filePath = Path.Combine(documentsPath, "Exp.txt");
-
-                        // Write the message to a text file
-                        using (StreamWriter writer = new StreamWriter(filePath, true))
-                        {
-                            writer.WriteLine($"{DateTime.Now} - {message}");
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                string message = ex.Message + "last Exp";
-
-                // Get the path to the Documents folder
-                string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-
-                // Specify the file path in the Documents folder
-                string filePath = Path.Combine(documentsPath, "shutdown_log4.txt");
-
-                // Write the message to a text file
-                using (StreamWriter writer = new StreamWriter(filePath, true))
-                {
-                    writer.WriteLine($"{DateTime.Now} - {message}");
-                }
-            }
-           
-        }
-
+        
 
         private static async void ConnectSignalRServer()
         {
             try
             {
                 //string signalRUrl = "https://localhost:44301/notifyclient";
-                //string signalRUrl = "https://stagapi.intimepros.com/notifyclient";
-
+                string signalRUrl = "https://stagapi.intimepro.io/notifyclient";
+                //string signalRUrl = "https://uatapi.intimepro.io/notifyclient";
+                //string signalRUrl = "https://api.intimepro.io/notifyclient",
                 //-------------------
-               SignalRConn = new HubConnectionBuilder()
-                  .WithUrl("https://uatapi.intimepro.io/notifyclient")
+                SignalRConn = new HubConnectionBuilder()
+                  .WithUrl(signalRUrl)
                   .Build();
 
                 SignalRConn.Closed += async (error) =>
@@ -377,45 +242,97 @@ namespace CalculateLeftTime
                     data.device = "W";
                     await SignalRConn.SendAsync("RegisterClientId", data);
                     System.Diagnostics.Debug.WriteLine("RegisterClientId");
+                    AddLogs("RegisterClientId");
                 };
 
                 try
                 {
                     await SignalRConn.StartAsync();
                     System.Diagnostics.Debug.WriteLine("StartAsync RegisterClientId");
-
+                    AddLogs("StartAsync RegisterClientId");
+                    TrigerSignalRServerUserStatus("SignalRAgentUserStatus");
+                    PostUserStatusToWeb(4);
                 }
                 catch (Exception ex)
                 {
                     System.Diagnostics.Debug.WriteLine("RegisterClientId" + ex);
+                    AddLogs("RegisterClientId" + ex);
                 }
                 //-------------------
             }
             catch (Exception ex)
             {
-                System.Diagnostics.Debug.WriteLine("StartAsync RegisterClientId" + ex);
+                System.Diagnostics.Debug.WriteLine(" RegisterClientId" + ex);
+                AddLogs("StartAsync RegisterClientId" + ex);
             }
         }
-
-        public void TrigerSignalRServerUserStatus(string SignalREvent)
+        public static void AddLogs(string message)
         {
             try
             {
+
+                // Get the path to the Documents folder
+                string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                // Specify the file path in the Documents folder
+                string filePath = Path.Combine(documentsPath, "IsSuccessStatusCode.txt");
+
+                // Check if the file exists
+                bool fileExists = File.Exists(filePath);
+
+                // Write the message to a text file
+                using (StreamWriter writer = new StreamWriter(filePath, true))
+                {
+                    // If the file already exists and it's not empty, move to the next line
+                    if (fileExists && new FileInfo(filePath).Length > 0)
+                    {
+                        writer.WriteLine();
+                    }
+                    writer.WriteLine($"{DateTime.Now} - {message}");
+                }
+            }
+            catch (Exception ex)
+            {
+                string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                // Specify the file path in the Documents folder
+                string filePath = Path.Combine(documentsPath, "IsSuccessStatusCode.txt");
+
+                // Write the message to a text file
+                using (StreamWriter writer = new StreamWriter(filePath, true))
+                {
+                    writer.WriteLine($"{DateTime.Now} - {ex.Message} ");
+                }
+            }
+        }
+        public static void TrigerSignalRServerUserStatus(string SignalREvent)
+        {
+            try
+            {
+                AddLogs("in TrigerSignalRServerUserStatus");
                 #region snippet_ConnectionOn
-                if (SignalRConn == null) return;
+                if (SignalRConn == null) 
+                {
+
+                    AddLogs("SignalRConn null TrigerSignalRServerUserStatus");
+                    return;
+                }
                 SignalRConn.On<UserStatusDto>(SignalREvent, (data) =>
                 {
                     Console.WriteLine("gfgggjfjhgjhghjgjhgjhgkg");
                     System.Diagnostics.Debug.WriteLine("gfgggjfjhgjhghjgjhgjhgkg");
+                    AddLogs("TrigerSignalRServerUserStatus");
                 });
+                AddLogs("endregion SignalRConn null TrigerSignalRServerUserStatus");
                 #endregion
             }
             catch (Exception ex)
             {
                 System.Diagnostics.Debug.WriteLine("TrigerSignalRServerUserStatus" + ex);
+                AddLogs("TrigerSignalRServerUserStatus" + ex);
             }
         }
-        public async Task<HttpResponseMessage> PostUserStatusToWeb()
+        public static async Task<HttpResponseMessage> PostUserStatusToWeb(int UserStatus)
         {
             try
             {
@@ -424,30 +341,18 @@ namespace CalculateLeftTime
                     UserStatusDto userStatusDto = new UserStatusDto();
                     userStatusDto.TenantId = tenantId;
                     userStatusDto.UserId = userId;
-                    userStatusDto.UserStatus = 5;
+                    userStatusDto.UserStatus = UserStatus;
 
                     await SignalRConn.InvokeAsync("SignalRetOrUpdateUserStatusFromAgent", userStatusDto);
                     System.Diagnostics.Debug.WriteLine("SignalRetOrUpdateUserStatusFromAgent");
-
-                    string message = "Signal R status send" + "";
-
-                    // Get the path to the Documents folder
-                    string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-
-                    // Specify the file path in the Documents folder
-                    string filePath = Path.Combine(documentsPath, "IsSuccessStatusCode.txt");
-
-                    // Write the message to a text file
-                    using (StreamWriter writer = new StreamWriter(filePath, true))
-                    {
-                        writer.WriteLine($"{DateTime.Now} - {message} ");
-                    }
+                    AddLogs("Signal R status send");
                     return new HttpResponseMessage();
                 }
             }
             catch (Exception ex)
             {
                 System.Diagnostics.Debug.WriteLine("SignalRetOrUpdateUserStatusFromAgent" + ex);
+                AddLogs("SignalRetOrUpdateUserStatusFromAgent" + ex);
             }
             return null;
         }
--- obj/Release/net7.0-windows/CalculateLeftTime.AssemblyInfo.cs
+++ obj/Release/net7.0-windows/CalculateLeftTime.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculateLeftTime")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7335bf557376d8cdb13adfef3cfb88ce9cfd83df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0d5583599fde9598361952698016d47547049dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculateLeftTime")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculateLeftTime")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
